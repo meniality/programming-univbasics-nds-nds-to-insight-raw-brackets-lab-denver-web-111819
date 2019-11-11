198aa31d87480292b9d0a2fdf88041c003ba68fc
patrick hoehn <patrh_99@hotmail.com>
2019-11-11 17:15:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,18 +5,9 @@ def directors_totals(nds)
  result = {}
  
  directors_index = 0
- 
-  while directors_index < nds.length do
+ while directors_index < nds.length do
+   result[]
     
-    gross_income = 0
-    movie_index = 0
-    while movie_index < nds[directors_index][:movies].length do
-      gross_income +=  nds[directors_index][:movies][movie_index][:worldwide_gross]  
-        
-      movie_index += 1
-    end
-    result[#{nds[directors_index][:name]}] = gross_income
-    directors_index += 1
-  end
+
   result
 end
