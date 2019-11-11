72cb95a691fc9bd20a16e0e1ae7606f278231cec
patrick hoehn <patrh_99@hotmail.com>
2019-11-11 16:45:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,6 +18,7 @@ def directors_totals(nds)
     directors_film_totals = #{directors_database[directors_index][:name]}: => gross_income
     directors_index += 1
   end
+  p directors_film_totals
   
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
