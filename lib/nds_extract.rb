81493bb7a739a3d852da28e851409d2e32bf28f2
patrick hoehn <patrh_99@hotmail.com>
2019-11-11 17:18:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,12 +2,13 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
- result = {}
+  result = {}
  
- directors_index = 0
- while directors_index < nds.length do
+  directors_index = 0
+  while directors_index < nds.length do
    result[nds[director_index][:name]] = 0
+  end
     
-
+  director_index +=1
   result
 end
